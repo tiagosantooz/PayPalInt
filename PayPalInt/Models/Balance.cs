a702706c54677fa35ab1dd755ec225407fb1c640
Tiago Alexandre Santos <tiago.a.santos@armis.pt>
2021-08-09 17:06:56 +0100

    Add project files.

---
+++ PayPalInt/Models/Balance.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PayPalInt
+{
+    public class Balance
+    {
+        public float ShowBalance { get; set; }
+
+        public string Currency { get; set; }
+
+        public string UserID { get; set; }
+
+        public string UserPassword { get; set; }
+    }
+}
